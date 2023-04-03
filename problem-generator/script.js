fbff8b36c5c417499ce89b5c6a61d761780adc1d
mimc <129696103+mimc-mock@users.noreply.github.com>
2023-04-03 10:07:19 -0700

    update script.js (v.1.0.4)

--- problem-generator/script.js
+++ problem-generator/script.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function loadText() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", './amc/' + year + '/' + test + '/' + problemNumber + '.txt');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + xhr.responseText;
-            } else {
-                document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + "Problem not Found";
-            }
-        };
-        xhr.send();
-    }
 
     document.querySelector('form#generate').addEventListener('submit', function () {
         let year = document.querySelector('#year').value;
         let test = document.querySelector('#test').value;
         let problemNumber = document.querySelector('#problemNumber').value;
-        
+
+        function loadText() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", './amc/' + year + '/' + test + '/' + problemNumber + '.txt');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + xhr.responseText;
+                } else {
+                    document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + "Problem not Found";
+                }
+            };
+            xhr.send();
+        }
+
         loadText();
         event.preventDefault();
     })
@@ -27,7 +28,20 @@ document.addEventListener('DOMContentLoaded', function () {
         let year = Math.floor(Math.random() * 21) + 2002;
         let test = tests[Math.floor(Math.random() * tests.length)];
         let problemNumber = Math.floor(Math.random() * 25) + 1;
-        
+
+        function loadText() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", './amc/' + year + '/' + test + '/' + problemNumber + '.txt');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + xhr.responseText;
+                } else {
+                    document.querySelector('#problem').innerHTML = `<h2>${year} ${test} Problem ${problemNumber}</h2>` + "Problem not Found";
+                }
+            };
+            xhr.send();
+        }
+
         loadText();
         event.preventDefault();
     })
